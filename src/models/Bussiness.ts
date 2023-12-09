5aebe4fab5fc6a7bf63f387cd7fbe0e0efd1eeca
martaguillemolmos <martetaguillem@gmail.com>
2023-12-09 19:41:57 +0100

    Feat: Create Bussiness model.

---
+++ src/models/Bussiness.ts
@@ -0,0 +1,46 @@
+import { IsString, MaxLength, MinLength, IsNumber, Max, Min, IsEmail, IsBoolean, IsEnum, IsDate } from "class-validator"
+
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class Bussiness extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id!: number
+  
+    @Column()
+    @IsString()
+    @MaxLength(50)
+    @MinLength(3)
+    name!: string
+  
+    @Column()
+    @IsString()
+    @MaxLength(250)
+    @MinLength(3)
+    description!: string
+
+    @Column()
+    @IsNumber()
+    @Max(999999999)
+    @Min(600000000)
+    phone!: number
+  
+    @Column()
+    @MaxLength (50)
+    @IsEmail()
+    email!: string
+  
+    @Column()
+    @IsString()
+    @MaxLength(100)
+    @MinLength(3)
+    ubication!: string
+  
+    @Column()
+    @IsDate()
+    created_at!: Date
+  
+    @Column()
+    @IsDate()
+    update_at!: Date
+}
