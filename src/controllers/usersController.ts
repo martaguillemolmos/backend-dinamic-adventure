36d68a87df6cda705425b8b88ae37098fb968960
martaguillemolmos <martetaguillem@gmail.com>
2023-12-13 11:10:51 +0100

    Feat: In userController, we create the route updateUser.

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -5,7 +5,6 @@ import jwt from "jsonwebtoken";
 import { validate } from "class-validator";
 import dayjs from "dayjs";
 
-
 // Register: Crear nuevos usuarios
 const createUser = async (req: Request, res: Response) => {
   try {
@@ -15,7 +14,7 @@ const createUser = async (req: Request, res: Response) => {
     //Creamos un objeto para la validación
     const Uservalidate = new Users();
     Uservalidate.name = name.trim();
-    console.log(Uservalidate.name)
+    console.log(Uservalidate.name);
     Uservalidate.surname = surname.trim();
     Uservalidate.phone = phone;
     Uservalidate.email = email;
@@ -48,7 +47,7 @@ const createUser = async (req: Request, res: Response) => {
       password: encryptedPassword,
     }).save();
     if (newUser) {
-    // Creamos el token
+      // Creamos el token
       const token = jwt.sign(
         {
           id: newUser.id,
@@ -75,133 +74,184 @@ const createUser = async (req: Request, res: Response) => {
 
 //Login
 const loginUser = async (req: Request, res: Response) => {
-    try {
-      // Recuperamos los datos guardados en body
-      const { email, password } = req.body;
-  
-      // Comprobamos que nos envían characteres.
-      if (
-        !req.body.email ||
-        !req.body.password ||
-        req.body.email.trim() === "" ||
-        req.body.password.trim() === ""
-      ) {
-        return res.json({
-          success: true,
-          message: "Introduce usuario y contraseña.",
-        });
-      }
-  
-      // Validación de que el email sea @
-      const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-      if (!emailRegex.test(email) || email.length == 0 || email.length > 50 ){
-        return res.json("Formato de email incorrecto. Recuerda: Número máx. de caracteres 50.")
-      }
-  
-      // Validación que el password contiene como mínimo y como máximo.
-      if(password.length < 6 || password.length >12) {
-        return res.json ("El password debe contener de 6 a 12 caracteres.")
-      }
-      //Consultar en BD si el usuario existe
-      const user = await Users.findOneBy({
-        email: email.trim(),
+  try {
+    // Recuperamos los datos guardados en body
+    const { email, password } = req.body;
+
+    // Comprobamos que nos envían characteres.
+    if (
+      !req.body.email ||
+      !req.body.password ||
+      req.body.email.trim() === "" ||
+      req.body.password.trim() === ""
+    ) {
+      return res.json({
+        success: true,
+        message: "Introduce usuario y contraseña.",
       });
-  
-      // En el caso que el usuario no sea el mismo
-      if (!user) {
-        return res.status(403).json("Usuario o contraseña incorrecta");
-      }
-      //Comprobamos si el usuario está activo
-      if (!user?.is_active) {
-        return res.status(404).json("Usuario o contraseña incorrecta");
-      }
-      //Si el usuario si es correcto, compruebo la contraseña
-      console.log(user.password);
-      if (bcrypt.compareSync(password.trim(), user.password)) {
-        //En caso de que hayamos verificado que el usuario es correcto y se corresponde a la contraseña que hemos indicado, generar token
-        const token = jwt.sign(
-          {
-            id: user.id,
-            role: user.role,
-            is_active: user.is_active,
-          },
-          process.env.JWT_SECRET as string,
-          {
-            expiresIn: "2h",
-          }
-        );
-        return res.json({
-          success: true,
-          message: `Bienvenid@ a tu perfil, ${user.name}`,
-          token: token,
-          name: user.name,
-        });
-      } else {
-        return res
-          .status(403)
-          .json({ message: "Usuario o contraseña incorrecta." });
-      }
-    } catch (error) {
-      return res.status(500).json(error);
     }
-  };
+
+    // Validación de que el email sea @
+    const emailRegex =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!emailRegex.test(email) || email.length == 0 || email.length > 50) {
+      return res.json(
+        "Formato de email incorrecto. Recuerda: Número máx. de caracteres 50."
+      );
+    }
+
+    // Validación que el password contiene como mínimo y como máximo.
+    if (password.length < 6 || password.length > 12) {
+      return res.json("El password debe contener de 6 a 12 caracteres.");
+    }
+    //Consultar en BD si el usuario existe
+    const user = await Users.findOneBy({
+      email: email.trim(),
+    });
+
+    // En el caso que el usuario no sea el mismo
+    if (!user) {
+      return res.status(403).json("Usuario o contraseña incorrecta");
+    }
+    //Comprobamos si el usuario está activo
+    if (!user?.is_active) {
+      return res.status(404).json("Usuario o contraseña incorrecta");
+    }
+    //Si el usuario si es correcto, compruebo la contraseña
+    console.log(user.password);
+    if (bcrypt.compareSync(password.trim(), user.password)) {
+      //En caso de que hayamos verificado que el usuario es correcto y se corresponde a la contraseña que hemos indicado, generar token
+      const token = jwt.sign(
+        {
+          id: user.id,
+          role: user.role,
+          is_active: user.is_active,
+        },
+        process.env.JWT_SECRET as string,
+        {
+          expiresIn: "2h",
+        }
+      );
+      return res.json({
+        success: true,
+        message: `Bienvenid@ a tu perfil, ${user.name}`,
+        token: token,
+        name: user.name,
+      });
+    } else {
+      return res
+        .status(403)
+        .json({ message: "Usuario o contraseña incorrecta." });
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 
 // Perfil
 const profileUser = async (req: any, res: Response) => {
-    try {
-      //Recuperamos el id del usuario a través del token.
-      const user = await Users.findOneBy({
-        id: req.token.id,
-      });
-      // Añadimos que, si el usuario en el momento desactive la cuenta, ya no se le permite acceder a su perfil.
-      if (!user) {
-        return res.status(403).json("Usuario no autorizado.");
-      }
-  
-      if (!user?.is_active) {
-        return res.status(404).json("Usuario no autorizado.");
-      }
-  
-      return res.json({
-        message: "Datos del perfil",
-        data: user,
-      });
-    } catch (error) {
-      return res.json({
-        succes: false,
-        message: "Usuario no autorizado.",
-        error: error,
-      });
+  try {
+    //Recuperamos el id del usuario a través del token.
+    const user = await Users.findOneBy({
+      id: req.token.id,
+    });
+    // Añadimos que, si el usuario en el momento desactive la cuenta, ya no se le permite acceder a su perfil.
+    if (!user) {
+      return res.status(403).json("Usuario no autorizado.");
     }
-  };
+
+    if (!user?.is_active) {
+      return res.status(404).json("Usuario no autorizado.");
+    }
+
+    return res.json({
+      message: "Datos del perfil",
+      data: user,
+    });
+  } catch (error) {
+    return res.json({
+      succes: false,
+      message: "Usuario no autorizado.",
+      error: error,
+    });
+  }
+};
+
+// Modificar la información del perfil.
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    let user;
+    if (   
+      req.token.is_active == true 
+    ) {
+      //Lógica para actualizar usuarios por su Id. Este lo recuperamos del token.
+       user = await Users.findOne({
+        where: { id: req.token.id },
+      });
+    } else {
+      return res.status(403).json({ message: "Usuario no autorizado" });
+    }
+
+    // Indicamos los datos que se pueden actualizar a través de esta ruta.
+    const { name, surname, phone, email} = req.body;
+    
+    // Validar el formato de los nuevos datos.
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (email !== undefined && email.trim() !=="" && !emailRegex.test(email) && (email.length == 0 || email.length > 50) ){
+      return res.json("Formato de email incorrecto. Recuerda: Número máx. de caracteres 50.")
+    }
+    if(name !== undefined && name.trim() !=="" && name.length >50) {
+      return res.json ("User: Número máx. de caracteres 50.")
+    }
+    if(surname !== undefined && surname.trim() !=="" && surname.length >50) {
+      return res.json ("User: Número máx. de caracteres 50.")
+    }
+    if(phone !== undefined && (phone >999999999 || phone < 600000000 )){
+      return res.json ("Introduce un número de 9 caracteres, puede empezar desde el 6.")
+    }
+   
+    //Comprobamos que el usuario exista
+    if (!user) {
+      return res.status(403).json({ message: "Usuario no encontrado" });
+    }
+
+    return res.json(`${user.name},tu información se ha actualizado con éxito.`);
+  } catch (error) {
+    console.log("error", error);
+    return res.json({
+      succes: false,
+      message: `No se ha podido actualizar la información.`,
+      error: error,
+    });
+  }
+};
 
 // Recuperar todos los usuarios
 const getAllUsers = async (req: Request, res: Response) => {
-    try {
-        // Recuperamos a todos los usuarios
-        const users = await Users.find();
-        // Comprobamos si hay usuarios registrados.
-        if (users.length == 0) {
-          return res.json({
-            success: true,
-            message: `Actualmente, no hay usuarios registrados.`
-          });
-        } else {
-          return res.json(
-            {
-                succes: true,
-                message: "Usuarios registrados.",
-                data: users,
-              }
-              );
-        }
-      } catch (error) {
-        return res.json({
-          succes: false,
-          message: "No hemos podido recuperar los usuarios",
-          error: error,
-        });
+  try {
+    // Recuperamos a todos los usuarios
+    const users = await Users.find();
+    // Comprobamos si hay usuarios registrados.
+    if (users.length == 0) {
+      return res.json({
+        success: true,
+        message: `Actualmente, no hay usuarios registrados.`,
+      });
+    } else {
+      return res.json({
+        succes: true,
+        message: "Usuarios registrados.",
+        data: users,
+      });
     }
+  } catch (error) {
+    return res.json({
+      succes: false,
+      message: "No hemos podido recuperar los usuarios",
+      error: error,
+    });
+  }
 };
 
-export {createUser, loginUser, profileUser, getAllUsers };
+
+export { createUser, loginUser, profileUser, getAllUsers, updateUser };
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,11 +1,19 @@
 import { Request, Response, Router } from "express";
-import { createUser, getAllUsers, loginUser, profileUser } from "../controllers/usersController";
+import { createUser, getAllUsers, loginUser, profileUser, updateUser } from "../controllers/usersController";
 import { auth } from "../middelware/auth";
 const router = Router ();
 
 export {router}
 
+//Rutas para Usuarios
+
+//Login:
 router.post ("/", loginUser);
+//Register: Crear un nuevo usuario.
 router.post ("/register", createUser);
+//Recuperar la información de todos los usuarios.
 router.get("/", getAllUsers);
+//Porfile: Recuperar toda la información del usuario.
 router.get("/profile", auth, profileUser);
+//Modificar la información del perfil.
+router.put ("/", auth, updateUser);
