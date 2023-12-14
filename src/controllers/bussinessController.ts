7383b3bcd0df9fb773223f49fa6a207c42d8a21b
martaguillemolmos <martetaguillem@gmail.com>
2023-12-14 12:47:50 +0100

    Feat: Create route in bussinessController, updateBussiness.

--- src/controllers/bussinessController.ts
+++ src/controllers/bussinessController.ts
@@ -54,9 +54,78 @@ const createBussiness = async (req: Request, res: Response) => {
       });
     }
   };
-  const updateBussiness = (req: Request, res: Response) => {
-    return res.send("Update");
+  const updateBussiness = async(req: Request, res: Response) => {
+    try {
+      //Recuperamos el id 
+      const bussinessId = req.body.id;
+      //Comprobamos que el id exista
+      const existBussiness = await Bussiness.findOneBy({
+        id: parseInt(bussinessId)
+      })
+      if (!existBussiness){
+        return res.json ("El id no existe")
+      }
+  
+      //Recuperamos la información que van a modificar
+      const { name, description, phone, email, ubication } = req.body;
+      console.log(req.body, "soy el body")
+  // Validar el formato de los nuevos datos.
+  const emailRegex =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+if (
+  email !== undefined &&
+  email.trim() !== "" &&
+  !emailRegex.test(email) &&
+  (email.length == 0 || email.length > 50)
+) {
+  return res.json(
+    "Formato de email incorrecto. Recuerda: Número máx. de caracteres 50."
+  );
+}
+if (name !== undefined && name.trim() !== "" && name.length > 50) {
+  return res.json("Número máx. de caracteres 50.");
+}
+if (description !== undefined && description.trim() !== "" && description.length > 250) {
+  return res.json("Número máx. de caracteres 250.");
+}
+if (phone !== undefined && (phone > 999999999 || phone < 600000000)) {
+  return res.json(
+    "Introduce un número de 9 caracteres, puede empezar desde el 6."
+  );
+}
+if (ubication !== undefined && ubication.trim() !== "" && ubication.length > 250) {
+  return res.json("Número máx. de caracteres 250.");
+}
+
+      //Actualizamos los datos
+     await Bussiness.update(
+        {
+          id : parseInt(bussinessId),
+        },
+        {
+          name,
+          description,
+          phone,
+          email,
+          ubication
+        }
+      );
+      return res.json ({
+        success: true,
+        message: "Actualizado",
+      })
+     
+  
+    } catch (error) {
+      console.log("error", error);
+      return res.json({
+        succes: false,
+        message: `No se ha podido actualizar la información.`,
+        error: error,
+      });
+    }
   };
+  
   const getBussinessById = (req: Request, res: Response) => {
     return res.send("By Id");
   };
